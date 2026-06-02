c204b3a4f61174a79da92bb33f0c79c80027fcfd
Gian <fabricio7ampuero@gmail.com>
2026-06-02 11:56:24 -0500

    actualizdo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export const About = () => (
   <section id="sobre-mi">
     <h2>Sobre mí</h2>
     <p style={{ lineHeight: '1.6', color: '#cbd5e1' }}>
-      Soy estudiante de Ingeniería de Sistemas de octavo ciclo, con interés en el desarrollo tecnológico, la gestión de datos y la solución de problemas mediante herramientas informáticas. Cuento con conocimientos en Python, SQL Server, HTML, CSS, JavaScript y Excel, así como experiencia desarrollando proyectos académicos y aplicaciones básicas. Me caracterizo por ser una persona analítica, responsable y con capacidad de aprendizaje continuo, siempre buscando mejorar mis habilidades técnicas y aportar soluciones eficientes en entornos tecnológicos. Actualmente, me encuentro enfocado en seguir fortaleciendo mis competencias en desarrollo de software, análisis de datos y tecnologías en la nube, con el objetivo de crecer profesionalmente en el área de tecnología 
+      Soy estudiante de Ingeniería de Sistemas de noveno ciclo, con interés en el desarrollo tecnológico, la gestión de datos y la solución de problemas mediante herramientas informáticas. Cuento con conocimientos en Python, SQL Server, HTML, CSS, JavaScript y Excel, así como experiencia desarrollando proyectos académicos y aplicaciones básicas. Me caracterizo por ser una persona analítica, responsable y con capacidad de aprendizaje continuo, siempre buscando mejorar mis habilidades técnicas y aportar soluciones eficientes en entornos tecnológicos. Actualmente, me encuentro enfocado en seguir fortaleciendo mis competencias en desarrollo de software, análisis de datos y tecnologías en la nube, con el objetivo de crecer profesionalmente en el área de tecnología 
     </p>
   </section>
 
